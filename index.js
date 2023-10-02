6436956a06039ff707ac18bf061f72a8faeef252
pavithradogiparthi <86222542+pavithradogiparthi@users.noreply.github.com>
2023-10-02 09:41:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-const CONNECTION_URL = 'mongodb+srv://pavithra:pavithra@pavithra.nzghozw.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://pavithradogiparthi:pavithra123@cluster0.ppdtk2g.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
